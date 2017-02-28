b52959d198e37172a98c029e2d84e0b601fbd176
Tomasz Dowgielewicz <td@atest.co.uk>
2017-02-28 16:52:25 +0100

    flatten working

--- main.py
+++ main.py
@@ -48,15 +48,106 @@ else:
 
 #save images for report purpose
 if REPORT:
-    img = cv2.imread('camera_calibration/calibration3.jpg')
+    img = cv2.imread('report/sample2.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite("report/before_calibration.png",gray)
+    cv2.imwrite("report/before_calibration_road.png",img)
     dst = cv2.undistort(img, camear_calibration['mtx'], camear_calibration['dist'], None, camear_calibration['mtx'])
-    cv2.imwrite("report/after_calibration.png", dst)
+    cv2.imwrite("report/after_calibration_road.png", dst)
 
 
 
+def flat_perspective(img):
+    #points taken manualy in paint
+    img_size = (400, 800)
+    offset = {'x':80,'y':10}
+    p1 = [610,440]
+    p4 = [373,623]
+    p3 = [1000,623]
+    p2 = [680,444]
 
+    src = np.float32([p1,p2,p3,p4])
+    dst = np.float32([[offset['x'], offset['x']], [img_size[0] - offset['x'], offset['y']],
+                      [img_size[0] - offset['x'], img_size[1] - offset['y']],
+                      [offset['x'], img_size[1] - offset['y']]])
+    M = cv2.getPerspectiveTransform(src, dst)
+    out = cv2.warpPerspective(img, M, img_size)
+    # plt.imshow(out)
+    # plt.show()
+    return out
+
+
+def s_treshording(img):
+
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    # H = hls[:, :, 0]
+    # L = hls[:, :, 1]
+    S = hls[:, :, 2]
+
+    thresh = (90, 255)
+    binary = np.zeros_like(S)
+    binary[(S > thresh[0]) & (S <= thresh[1])] = 1
+    plt.imshow(binary)
+    plt.show()
+
+    return binary
+
+def h_treshording(img):
+
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    H = hls[:, :, 0]
+
+    thresh = (90, 255)
+    binary = np.zeros_like(H)
+    binary[(H > thresh[0]) & (H <= thresh[1])] = 1
+    # plt.imshow(out)
+    # plt.show()
+
+    return binary
+
+
+def image_preprocesor(img):
+    #1 Fix camera disortion
+    img = cv2.undistort(img, camear_calibration['mtx'], camear_calibration['dist'], None, camear_calibration['mtx'])
+
+    return img
+
+#report flatening images
+if REPORT:
+    img = cv2.imread('report/sample3.png')
+    flat = flat_perspective(img)
+    cv2.imwrite("report/flat_sample3.png", flat)
+
+
+if REPORT:
+    images = glob.glob('report/sample*.png')
+    for image_path in images:
+        img = cv2.imread(image_path)
+        out = s_treshording(img)
+        new_path = image_path.replace('sample','s_treshold_sample_')
+        cv2.imwrite(new_path, out)
+
+
+
+#cap = cv2.VideoCapture('project_video.mp4')
+#
+# while(cap.isOpened()):
+#
+#     ret, frame = cap.read()
+#     if ret==True:
+#
+#
+#         # write the flipped frame
+#         #out.write(frame)
+#
+#         cv2.imshow('frame',frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             cv2.imwrite("report/sample.png", frame)
+#             break
+#     else:
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
 
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-##Writeup Template
-###You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
+## Writeup Template
+### You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
 
 ---
 
@@ -16,11 +16,8 @@ The goals / steps of this project are the following:
 * Warp the detected lane boundaries back onto the original image.
 * Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
 
-TTTTTTTTUUTTAJ#########################################
 
-[//]: # (Image References)
 
-<img src="https://github.com/muncz/sdc_p4/blob/master/report/before_calibration.png" width="120">
 
 [image2]: ./test_images/test1.jpg "Road Transformed"
 [image3]: ./examples/binary_combo_example.jpg "Binary Example"
@@ -30,15 +27,23 @@ TTTTTTTTUUTTAJ#########################################
 [video1]: ./project_video.mp4 "Video"
 
 ## [Rubric](https://review.udacity.com/#!/rubrics/571/view) Points
-###Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
+### Here I will consider the rubric points individually and describe how I addressed each point in my implementation.
 
 ---
-###Writeup / README
+### Writeup / README
 
-####1. Provide a Writeup / README that includes all the rubric points and how you addressed each one.  You can submit your writeup as markdown or pdf.  [Here](https://github.com/udacity/CarND-Advanced-Lane-Lines/blob/master/writeup_template.md) is a template writeup for this project you can use as a guide and a starting point.  
+#### 1. Provide a Writeup / README that includes all the rubric points and how you addressed each one.  You can submit your writeup as markdown or pdf.  [Here](https://github.com/udacity/CarND-Advanced-Lane-Lines/blob/master/writeup_template.md) is a template writeup for this project you can use as a guide and a starting point.
 
 You're reading it!
-###Camera Calibration
+### Camera Calibration
+Before Calibration:
+
+<img src="https://github.com/muncz/sdc_p4/blob/master/report/before_calibration.png" width="400">
+
+After Calibration:
+
+<img src="https://github.com/muncz/sdc_p4/blob/master/report/after_calibration.png" width="400">
+
 
 ####1. Briefly state how you computed the camera matrix and distortion coefficients. Provide an example of a distortion corrected calibration image.
 
